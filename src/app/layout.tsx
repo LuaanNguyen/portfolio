606b6cec056fdc26a9ed900a8b2fc30c38326220
LuaanNguyen <luanthiennguyen2003@gmail.com>
2025-01-16 14:32:32 -0700

    Removed metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/luan.jpg" sizes="any" />
         <link
           rel="icon"
           href="/icon?<generated>"
