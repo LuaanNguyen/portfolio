5f939328c42a94fb7987c0337c19275204c7b899
LuaanNguyen <luanthiennguyen2003@gmail.com>
2026-03-11 14:15:55 -0700

    spotify: change album

--- src/app/components/sections/SpotifyAlbum.tsx
+++ src/app/components/sections/SpotifyAlbum.tsx
@@ -5,7 +5,7 @@ const SpotifyAlbum: React.FC = () => {
     <div className="max-md:hidden max-lg:col-span-1 max-lg:row-span-1 col-span-2 row-span-2 col-start-5 row-start-7 bg-spotify-black rounded-xl">
       <iframe
         className="w-full h-full rounded-xl"
-        src="https://open.spotify.com/embed/album/3Dx1IXTwJVIMkEzVMSSSrp?utm_source=generator"
+        src="https://open.spotify.com/embed/album/3EGfky2LBE7t8mWKtHzNeg?utm_source=generator"
         title="Spotify album player"
         allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
         loading="lazy"
