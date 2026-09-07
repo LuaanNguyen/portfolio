70e385f3047ae0315a862de4556832b9cefd7d7f
LuaanNguyen <luanthiennguyen2003@gmail.com>
2026-09-07 14:52:15 -0700

    chore: removed eBay in ProfileCard

--- src/app/components/sections/ProfileCard.tsx
+++ src/app/components/sections/ProfileCard.tsx
@@ -43,16 +43,6 @@ function PersonalStatement() {
         here stays with you.
         <br />
         <br />
-        I&apos;m an software engineer intern on the Public APIs & Developer
-        Ecosystem team at{" "}
-        <a
-          href="https://www.ebay.com/"
-          className="text-spotify-green underline"
-        >
-          eBay.
-        </a>{" "}
-        <br />
-        <br />
         <span>
           My interests center on{" "}
           <span className="text-spotify-green">
